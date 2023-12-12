78b634a6d668d160723758fef63d95da0911c81a
Pierrick Viot-Launier <Pierrick.Violau@gmail.com>
2023-12-11 22:36:15 -0500

    contacts page created

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -6,12 +6,12 @@ export const meta = () => {
 
 export default function Index() {
   return (
-    <main>
+    <main id="content">
       <h1>Gestion de contacts</h1>
 
       <p>Cette application vous permet de gérer votre liste de contacts.</p>
 
-      <p>
+      <p id="cta">
         <Link to="/contacts">Voir mes contacts</Link>
       </p>
     </main>
---
+++ app/routes/contacts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ContactsPage() {
+  return (
+    <main>
+      <h1>Mes contacts</h1>
+    </main>
+  );
+}
